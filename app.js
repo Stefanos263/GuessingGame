651958fac9bbc2ddb9fdcd2d437487d55875993c
Stefanos263 <stefanoskempas26@gmail.com>
2023-10-04 10:35:56 +0300

    bug fixes

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ while (!maximum){
 }
 const targetNum = Math.floor(Math.random() * maximum) +1 ;
 console.log(targetNum);
-let guess = parseInt(prompt("Enter your first guess!"));
+let guess =prompt("Enter your first guess! (Type `q` to quit!)");
 let attempts = 1;
 while ( parseInt(guess!==targetNum)){
     if (guess === "q")break;
-    attempts++
+    guess =parseInt(guess);
     if(guess>targetNum){
        guess =prompt("Too High!Enter a new guess:");
-    }else{
+       attempts++;
+    }else if (guess < targetNum){
         guess =prompt("Too Low!Enter a new guess:");
+        attempts++;
+    }else {
+      guess=prompt("invalid guess. please enter a nymber or `q` to quit!")   
     }
 }
 if(guess === "q"){
